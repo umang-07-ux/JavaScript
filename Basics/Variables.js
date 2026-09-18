dc52b15ea31920680e5cbbd5458fbddacf636900
Umang Srivastava <umang2897@gmail.com>
2026-09-18 14:52:15 +0530

    Initial commit

---
+++ Basics/Variables.js
@@ -0,0 +1,14 @@
+
+//'const' is a keyword jisko hum variable ko declare karne ke liye use karte hai. 
+//'const' se declare kiya gaya variable ka value change nahi ho sakta.
+//Example:
+// accountId=2, yeh code error dega kyuki accountId ko 'const' se declare kiya gaya hai aur iska value 
+// change nahi ho sakta.
+const accountId=144553
+
+//'let'
+let accountEmail="umang@gmail.com"
+var accountPassword="123456"
+accountCity="Lucknow"
+
+console.log(accountId);  //Output: 144553
---
+++ Basics/test.js
@@ -0,0 +1,2 @@
+console.log("Umang");  //'console.log' ek function hai jisko hum console me output print karne ke liye use
+                       // karte hai.
