857c28153c07ae145c284370d498e798ef8f4d12
Umang Srivastava <umang2897@gmail.com>
2026-09-23 16:55:33 +0530

    Numbers

---
+++ Basics/NumsAndMaths.js
@@ -0,0 +1,40 @@
+const score=400
+console.log(score);
+
+
+const balance=new Number(100) // Output:[Number: 100]
+console.log(balance);
+
+console.log(balance.toString());  // Convert number into string.
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); // Output:100.00
+                                 // It gives me decimal value jisme decimal ke baad 2 hi value tak answer hoga.
+
+const otherNumber=12.8966
+console.log(otherNumber.toPrecision(3)); //Output:12.9
+                                         //Return the precise value 
+                                         //Returns a string containing a number represented either in exponential or fixed-point notation with a specified number of digits.
+const number2=123.8966
+console.log(number2.toPrecision(3)); //Output:124
+             
+const number3=1123.8966
+console.log(number3.toPrecision(3)); //Output:1.12e+3
+
+const number4=1000000
+console.log(number4.toLocaleString()); //Output:1,000,000
+                                       //Used to provide commas in the number but using it alone will give answer in US standards.
+console.log(number4.toLocaleString('en-IN')); //Output:10,00,000
+                                              //This will going to give answer according to INDIAN standard.
+
+const number5=Number.MAX_VALUE
+console.log(number5); //Gives max value.
+
+const number6=Number.MIN_VALUE
+console.log(number6); //Gives min value.
+
+
+
+                                              
+
+
+
