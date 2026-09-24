aa2f4e2c3b4bc530ff109c7cab09ffa6fa62617b
Umang Srivastava <umang2897@gmail.com>
2026-09-24 16:10:55 +0530

    Test

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,2 +1,3 @@
-console.log("Umang");  //'console.log' ek function hai jisko hum console me output print karne ke liye use
-                       // karte hai.
+console.log("Umang");  /*'console.log' ek function hai jisko hum console me output print karne ke liye use
+                          karte hai.
+                       */
