d054d67ff15b67bedb2da3f8a06a2268a1af7308
Umang Srivastava <umang2897@gmail.com>
2026-09-25 21:57:33 +0530

    Functions in JS

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -16,7 +16,7 @@ function addTwoNumbers(number1 , number2){
     console.log(number1+number2);
 }
 
-//Here '3 , 4' are arguments.
+// //Here '3 , 4' are arguments.
 addTwoNumbers(3,4)  //Output:7
 addTwoNumbers(3,"4") //Output:34
 addTwoNumbers(3,"a") //Output:3a
@@ -32,3 +32,27 @@ function addTwoNumbers2(number1 , number2){
 }
 const result2=addTwoNumbers2(3,5)  //Now i will get the answer which will be going to get stored in 'result2' 
 console.log("Result:",result2); //Output:8
+
+
+//Iss function mei if statement lagaya hai check karne ke liye ki agar function calling ke samay empty hai toh undefined answer dega toh agar username === undefined hai toh -- 
+function loginUserMessage(username){
+    if(username === undefined) {    //We can also write if(!username)
+        console.log("Please enter the username");
+        return  //this return is to come out of the function if the 'if' statement is true.
+    }
+    return `${username} just logged in.`
+}
+// console.log(loginUserMessage("Umang"));  //Output:Umang just logged in.
+console.log(loginUserMessage());  /*Output:Please enter the username
+                                           undefined
+                                  */
+
+function myName(name="Umang"){
+    if(!name) {        //This if statement will not be going to get executed in any case as we have given a sure value to 'name' in the function.
+        console.log("Please enter any name");
+        return
+    }
+    return `My name is ${name}`
+}
+console.log(myName());
+
